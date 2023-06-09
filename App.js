1076e39eedfa1171d7e84c593b774b6db47dccb4
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-09 11:27:22 +0530

    home screen up

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack=createNativeStackNavigator()
 
@@ -12,21 +13,10 @@ export default function App() {
     <NavigationContainer>
     <TailwindProvider>
       <Stack.Navigator>
-    <View style={styles.container}>
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Stack.Screen name="Home" component={HomeScreen}/>
     </Stack.Navigator>
     </TailwindProvider>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
