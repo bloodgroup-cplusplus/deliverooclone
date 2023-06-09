e19b6950098b8b0192481f1bcdcf65c714726ca1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-09 11:32:05 +0530

    added useNavigation useLayoutEffect and SafeArea view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
+    const navigation=useNavigation();
+    // as soon as the screnn mounts do something 
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerShown:false, // we don't want the huge home page on our page on load
+        })
+        
+
+    },[])
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
   )
 }
 
