d5638e09aa75df735bbbce82b833c582d2be6df4
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-09 11:47:52 +0530

    added react native icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native'
+import { Image } from 'react-native'
 
 const HomeScreen = () => {
     const navigation=useNavigation();
@@ -15,7 +16,23 @@ const HomeScreen = () => {
     },[])
   return (
     <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+      <Text className="text-red-500"></Text>
+      {/*Header*/}
+      <View className="flex-row pb-3 items-center mx-4  space-x-2">
+        <Image
+        source={{
+            uri: "https://links.papareact.com/wru"
+        }}
+        className="h-7 w-7 bg-gray-300 p-4 rounded-full" 
+        />
+        {/* everything is flex column in react native */}
+        <View>
+            <Text className="font-bold text-gray-400 text-x5">Deliver Now</Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+        </View>
+
+
+      </View>
     </SafeAreaView>
   )
 }
