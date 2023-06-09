d924d37d50b58cd560c931207509e1d7fe63ff2c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-09 11:55:23 +0530

    added background button in the app

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,7 +21,7 @@ const HomeScreen = () => {
 
     },[])
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className="text-red-500"></Text>
       {/*Header*/}
       <View className="flex-row pb-3 items-center mx-4  space-x-2">
@@ -32,12 +32,14 @@ const HomeScreen = () => {
         className="h-7 w-7 bg-gray-300 p-4 rounded-full" 
         />
         {/* everything is flex column in react native */}
-        <View>
+        <View className="flex-1">
             <Text className="font-bold text-gray-400 text-x5">Deliver Now</Text>
             <Text className="font-bold text-xl">Current Location
             <ChevronDownIcon size={20} color="#00CCBB"/>
             </Text>
         </View>
+
+        <UserIcon size={35} clolor="#00CCBB"/>
 
 
       </View>
