435fefd9f335e850f33c3232831d0d0503ce2469
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-09 11:14:29 +0530

    wrapping inside NavigationContainer TailwindProvider and Stack.Navigator

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {creatNativeStackNavigator} from "@react-navigation/native-stack"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack=createNativeStackNavigator()
+
 
 export default function App() {
   return (
+    <NavigationContainer>
     <TailwindProvider>
+      <Stack.Navigator>
     <View style={styles.container}>
       <Text className="text-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Stack.Navigator>
     </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
